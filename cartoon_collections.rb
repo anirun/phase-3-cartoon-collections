c508f0b3db3896bc4c719518c285830bcb01364e
Nurina Junus <nurina.junus@gmail.com>
2022-02-12 20:38:36 -0700

    commit tree 3d4bf936d3f29592d6098a573c6d19f59190f2e3

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,33 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+try = ["a", "b", "c"]
+
+def roll_call_dwarves(array)
+  array.each_with_index { |name, index| puts "#{index + 1}. #{name}" }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  capitalized_array = array.map { |name| name.capitalize + "!" }
+  return capitalized_array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.any? { |call| call.length > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  total_indexes = cheese_types.length - 1
+  
+  cheese_index = 0
+  while cheese_index < total_indexes
+    there_is_cheese = array.include?(cheese_types[cheese_index])
+    if there_is_cheese == true
+      return cheese_types[cheese_index]
+    elsif there_is_cheese == false
+    cheese_index += 1
+    end
+  end
+  
 end
